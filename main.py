bcfc813b684431371faa5e7328b9218dd5d45d7d
CatsEleven <hatijolove@gmail.com>
2025-02-20 03:26:36 +0900

    image center

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import TkEasyGUI as eg
 import pyautogui as pag
 
 scr_w, scr_h = pag.size()
+img_pad = (scr_w - 288) / 2
 
 instructions = (
     "・QRコードをスマートフォンで読み取って、集計システムにログインしてください（http://scrs0627.cc.ag.aoyama.ac.jp/）。\n"
@@ -13,11 +14,11 @@ instructions = (
 layout = [
     [eg.Text("AIM Commons利用者集計システム Ver.3", font=("Helvetica", 32), pad=((0, 0), (50, 30)))],
     [ 
-        eg.Text("",pad=((550, 0), (0, 30))),
+        eg.Text("",pad=((img_pad, 0), (0, 30))),
         eg.Image("qr.png")
     ],
     [eg.Label(text=instructions, font=("Helvetica", 16), pad=((0, 0), (0, 30)))],
-    [eg.Button("送信完了", size=(200, 2), font=("Helvetica", 40))],
+    [eg.Button("送信完了", size=(scr_w, 3), font=("Helvetica", 40))],
 ]
 
 window = eg.Window(title="AIM Commons利用者集計システム Ver.3", layout=layout, size=(scr_w, scr_h), no_titlebar=True, keep_on_top=True)
